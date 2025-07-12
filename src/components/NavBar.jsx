11fd1298254aff412c942de194aa2e90fc60d369
szilvia-barath <szilvia.barath2@unibo.it>
2025-07-12 17:17:21 +0200

    links fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,10 +17,10 @@ export default function Navbar({ active, onNavClick }) {
   };
 
   return (
-    <nav className="fixed top-0 left-0 z-50 w-full bg-[#0e0e0e] text-bone border-r-4 border-electric md:w-20 md:h-screen md:flex md:flex-col md:items-center md:border-b-0">
+    <nav className="fixed top-0 left-0 z-50 w-full bg-[#0e0e0e] text-bone border-b-2 border-b-electric  md:w-20 md:h-screen md:flex md:flex-col md:items-center md:border-b-0">
       {/* Desktop Nav */}
       <div className="flex-col items-center hidden gap-6 py-6 md:flex">
-        <div className="w-8 h-8 rounded-full bg-electric/10"><img src="./icon_scott.svg"></img></div>
+        <div className="w-8 h-8 rounded-full bg-electric/50 hover: animate-pulse"><a href="hero"><img  src="./icon_scott.svg"/></a></div>
         <div className="flex flex-col gap-4 mt-8 font-mono text-xs text-left">
           {navItems.map((item) => (
             <button
@@ -37,20 +37,22 @@ export default function Navbar({ active, onNavClick }) {
       </div>
 
       {/* Mobile Header */}
-      <div className="flex items-center justify-between px-4 py-3 md:hidden">
-        <div className="font-mono text-lg">SB</div>
+      <div className="flex items-center justify-between px-4 py-3 border-b-electric md:hidden">
+        <div className="font-mono text-lg"><a href='hero'><img className="w-8 h-8 hover: animate-pulse " src="./icon_scott.svg"/></a></div>
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-2xl focus:outline-none"
           aria-label="Toggle menu"
         >
-          ♡
+
+ <b>┊</b>
+
         </button>
       </div>
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden absolute top-full left-0 w-full bg-[#1a2129] px-4 py-6 flex flex-col gap-4 font-mono text-sm border-t border-electric z-[1001]">
+        <div className="md:hidden absolute top-full left-0 w-full bg-[#1a2129] px-4 py-6 flex flex-col gap-4 font-mono text-sm  z-[1001]">
           {navItems.map((item) => (
             <button
               key={item.id}
